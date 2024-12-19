b2c5e4d76a9060ebfda7366ef21018ccce6c8626
Addy Pathania <aditya.pathania22@gmail.com>
2024-12-19 15:32:50 -0500

    remove extra templates and intializers

--- packages/create-sitecore-jss/src/templates/angular/src/app/routing/scripts/scripts.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ScriptsComponent } from './scripts.component';
-
-@NgModule({
-  exports: [ScriptsComponent],
-  declarations: [ScriptsComponent],
-})
-export class ScriptsModule {}
