ad771eda9bb4427c944d68ff3083a3bfcc04d876
yavorsk <yavor.krastev@sitecore.com>
2025-01-31 17:29:14 +0200

    update scaffold-samples

--- scripts/scaffold-samples.js
+++ scripts/scaffold-samples.js
@@ -2,13 +2,13 @@ const chalk = require('chalk');
 
 const { initialize } = require('../packages/create-sitecore-jss/dist/initialize');
 const samplesToScaffold = require('./samples.json');
-const { getAppName } = require('./utils');
+const { getAppFolder } = require('./utils');
 
 for (const sample of samplesToScaffold) {
-  const appName = getAppName(sample.args);
-  sample.args.destination = `./samples/${appName}`;
-  sample.args.hostName = `${appName}.jss.localhost`;
-  console.log(chalk.green(`Initializing sample ${appName} ...`));
+  const appFolder = getAppFolder(sample.args);
+  sample.args.destination = `./samples/${appFolder}`;
+  sample.args.hostName = `${appFolder}.jss.localhost`;
+  console.log(chalk.green(`Initializing sample ${appFolder} ...`));
   // we need to keep noInstall as true - otherwise both yarn install and lint will execute
   // we run lint separately in the azure pipeline
   let scaffoldArgs = {
