8d3ad945fbcf59ff8b0a58e58f2ede8e40267c23
Addy Pathania <aditya.pathania@sitecore.com>
2025-02-07 13:16:33 -0500

    update cli directory name

--- packages/cli/src/bin/jss.ts
+++ packages/cli/src/bin/jss.ts
@@ -6,7 +6,7 @@ import processEnv from '../utils/process-env';
 /**
  * Resolves and executes the locally installed version of the JSS CLI.
  */
-resolve('@sitecore-jss/sitecore-jss-cli', { basedir: process.cwd() }, (_, projectLocalCli) => {
+resolve('@sitecore-content-sdk/cli', { basedir: process.cwd() }, (_, projectLocalCli) => {
   // No error implies a projectLocalCli, which will load whatever
   // version of jss-cli you have installed in a local package.json
   const cli = require(projectLocalCli as string).default;
