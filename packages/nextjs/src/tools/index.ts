89b644ba56b1f74354122421beacd0584a4e0310
yavorsk <yavor.krastev@sitecore.com>
2025-03-11 12:38:14 +0200

    adjust references; remove dev tools dependency in template; add component-props.loader to nextjs package

--- packages/nextjs/src/tools/index.ts
+++ packages/nextjs/src/tools/index.ts
@@ -2,5 +2,11 @@ export {
   generateSites,
   GenerateSitesConfig,
   generateMetadata,
+  ComponentFile,
+  PackageDefinition,
+  getComponentList,
+  PluginDefinition,
+  generatePlugins,
+  ModuleType,
 } from '@sitecore-content-sdk/core/tools';
 export * from './templating';
