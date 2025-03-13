9963892d3cb5bd69530bab8ae126fc5f122a6aed
yavorsk <yavor.krastev@sitecore.com>
2025-03-13 17:31:15 +0200

    initialize siteinfoservice in generateSites instead of in cli config

--- packages/create-sitecore-jss/src/templates/nextjs/sitecore.cli.config.ts
+++ packages/create-sitecore-jss/src/templates/nextjs/sitecore.cli.config.ts
@@ -1,7 +1,5 @@
 import config from './sitecore.config';
 import { defineCliConfig } from '@sitecore-content-sdk/nextjs/config';
-import { createGraphQLClientFactory } from '@sitecore-content-sdk/nextjs/client';
-import { GraphQLSiteInfoService } from '@sitecore-content-sdk/nextjs';
 import { generateSites, generateMetadata } from '@sitecore-content-sdk/nextjs/tools';
 
 export default defineCliConfig({
@@ -9,19 +7,7 @@ export default defineCliConfig({
     commands: [
       generateMetadata(),
       generateSites({
-        multisiteEnabled: config.multisite.enabled,
-        defaultSite: {
-          name: config.defaultSite,
-          language: config.defaultLanguage,
-          hostName: '*',
-        },
-        siteInfoService: new GraphQLSiteInfoService({
-          clientFactory: createGraphQLClientFactory({
-            api: config.api,
-            retries: config.retries.count,
-            retryStrategy: config.retries.retryStrategy,
-          }),
-        }),
+        scConfig: config,
       }),
     ],
   },
