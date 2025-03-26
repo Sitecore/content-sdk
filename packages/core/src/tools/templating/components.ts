4aeff5d92edd55205e28ac71daccd662cb1fb81f
Artem Alexeyenko <ala@sitecore.net>
2025-03-25 20:38:13 -0400

    cleanup core

--- packages/core/src/tools/templating/components.ts
+++ packages/core/src/tools/templating/components.ts
@@ -21,30 +21,25 @@ export interface PackageDefinition {
 }
 
 /**
- * Get list of components from @var paths
+ * Get list of components from @var path
  * Returns a list of components in the following format:
  * {
  *  path: 'path/to/component',
  *  componentName: 'ComponentName',
  *  moduleName: 'ComponentName'
  * }
- * @param {string[]} paths path to search
+ * @param {string} path path to search
  */
-export function getComponentList(paths: string[]): ComponentFile[] {
-  const components = paths.reduce((acc, path) => {
-    acc.push(
-      ...getItems<ComponentFile>({
-        path,
-        resolveItem: (path, name) => ({
-          path: `${path}/${name}`,
-          componentName: name,
-          moduleName: name.replace(/[^\w]+/g, ''),
-        }),
-        cb: (name) => console.debug(`Registering JSS component ${name}`),
-      })
-    );
-    return acc;
-  }, [] as ComponentFile[]);
+export function getComponentList(path: string): ComponentFile[] {
+  const components = getItems<ComponentFile>({
+    path,
+    resolveItem: (path, name) => ({
+      path: `${path}/${name}`,
+      componentName: name,
+      moduleName: name.replace(/[^\w]+/g, ''),
+    }),
+    cb: (name) => console.debug(`Registering JSS component ${name}`),
+  });
 
   return components;
 }
