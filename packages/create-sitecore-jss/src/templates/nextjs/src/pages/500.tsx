66eee34799f2fe68db5ce6025f2087c9390b0d4c
Artem Alexeyenko <ala@sitecore.net>
2025-05-01 15:54:42 -0400

    update nextjs sample

--- packages/create-sitecore-jss/src/templates/nextjs/src/pages/500.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs/src/pages/500.tsx
@@ -5,6 +5,7 @@ import { GetStaticProps } from 'next';
 import scConfig from 'sitecore.config';
 import client from 'lib/sitecore-client';
 import components from 'lib/component-map';
+import { JSX } from 'react';
 
 /**
  * Rendered in case if we have 500 error
