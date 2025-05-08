ac4aae4306547b0cea847e9aa34166cd296a6970
illiakovalenko <illia.kovalenko@sitecore.com>
2025-05-08 10:52:05 +0300

    [next.js] Rework and simplify .env

--- packages/create-sitecore-jss/src/templates/nextjs/src/pages/500.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs/src/pages/500.tsx
@@ -38,7 +38,7 @@ const Custom500 = (props: SitecorePageProps): JSX.Element => {
 export const getStaticProps: GetStaticProps = async (context) => {
   let resultErrorPages: ErrorPages | null = null;
 
-  if (process.env.DISABLE_SSG_FETCH?.toLowerCase() !== 'true') {
+  if (!scConfig.disableStaticPaths) {
     try {
       resultErrorPages = await client.getErrorPages({
         site: scConfig.defaultSite,
