006acc390ef538efbc0c0851914a8345bf54fd18
yavorsk <yavor.krastev@sitecore.com>
2025-05-08 20:40:24 +0300

    update comments

--- packages/create-sitecore-jss/src/templates/nextjs/sitecore.config.ts
+++ packages/create-sitecore-jss/src/templates/nextjs/sitecore.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@sitecore-content-sdk/nextjs/config';
 /**
  * @type {import('@sitecore-content-sdk/nextjs/config').SitecoreConfig}
- * See documentation about defineConfig here https://doc.sitecore.com/xmc/en/developers/content-sdk/the-sitecore-configuration-file.html
+ * See the documentation for `defineConfig`:
+ * https://doc.sitecore.com/xmc/en/developers/content-sdk/the-sitecore-configuration-file.html
  */
 export default defineConfig({});
