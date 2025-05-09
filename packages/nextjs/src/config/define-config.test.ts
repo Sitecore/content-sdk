7d6e4b8ea7af5a18de1c258a8acc6bd5f4b9592f
yavorsk <yavor.krastev@sitecore.com>
2025-05-09 11:59:13 +0300

    consolidate unit tests

--- packages/nextjs/src/config/define-config.test.ts
+++ packages/nextjs/src/config/define-config.test.ts
@@ -9,62 +9,6 @@ describe('defineConfig', () => {
     defaultLanguage: 'en',
   });
 
-  describe('disableStaticPaths', () => {
-    describe('environment variable is not set', () => {
-      it('should default to false', () => {
-        const config = defineConfig(defaultConfig());
-        expect(config.disableStaticPaths).to.equal(false);
-      });
-
-      it('should use the value from the config', () => {
-        const config = defineConfig({
-          disableStaticPaths: false,
-          ...defaultConfig(),
-        });
-        expect(config.disableStaticPaths).to.equal(false);
-      });
-    });
-
-    describe('environment variable is set', () => {
-      afterEach(() => {
-        delete process.env.DISABLE_SSG_FETCH;
-      });
-
-      it('should return true when DISABLE_SSG_FETCH is set to true', () => {
-        process.env.DISABLE_SSG_FETCH = 'true';
-
-        const config = defineConfig({
-          disableStaticPaths: false,
-          ...defaultConfig(),
-        });
-
-        expect(config.disableStaticPaths).to.equal(true);
-      });
-
-      it('should return false when DISABLE_SSG_FETCH is set to false', () => {
-        process.env.DISABLE_SSG_FETCH = 'false';
-
-        const config = defineConfig({
-          disableStaticPaths: true,
-          ...defaultConfig(),
-        });
-
-        expect(config.disableStaticPaths).to.equal(false);
-      });
-
-      it('should return false when DISABLE_SSG_FETCH is set to any other value', () => {
-        process.env.DISABLE_SSG_FETCH = 'some-other-value';
-
-        const config = defineConfig({
-          disableStaticPaths: true,
-          ...defaultConfig(),
-        });
-
-        expect(config.disableStaticPaths).to.equal(false);
-      });
-    });
-  });
-
   describe('getNextFallbackConfig', () => {
     describe('config.api.edge.contextid', () => {
       describe('environment variable is not set', () => {
@@ -365,6 +309,62 @@ describe('defineConfig', () => {
         it('should use the env var if config value not present', () => {
           const config = getNextFallbackConfig(defaultConfig());
           expect(config.personalize?.scope).to.equal('custom-env-scope');
+        });
+      });
+    });
+
+    describe('config.disableStaticPaths', () => {
+      describe('environment variable is not set', () => {
+        it('should default to false', () => {
+          const config = defineConfig(defaultConfig());
+          expect(config.disableStaticPaths).to.equal(false);
+        });
+
+        it('should use the value from the config', () => {
+          const config = defineConfig({
+            disableStaticPaths: false,
+            ...defaultConfig(),
+          });
+          expect(config.disableStaticPaths).to.equal(false);
+        });
+      });
+
+      describe('environment variable is set', () => {
+        afterEach(() => {
+          delete process.env.DISABLE_SSG_FETCH;
+        });
+
+        it('should return true when DISABLE_SSG_FETCH is set to true', () => {
+          process.env.DISABLE_SSG_FETCH = 'true';
+
+          const config = defineConfig({
+            disableStaticPaths: false,
+            ...defaultConfig(),
+          });
+
+          expect(config.disableStaticPaths).to.equal(true);
+        });
+
+        it('should return false when DISABLE_SSG_FETCH is set to false', () => {
+          process.env.DISABLE_SSG_FETCH = 'false';
+
+          const config = defineConfig({
+            disableStaticPaths: true,
+            ...defaultConfig(),
+          });
+
+          expect(config.disableStaticPaths).to.equal(false);
+        });
+
+        it('should return false when DISABLE_SSG_FETCH is set to any other value', () => {
+          process.env.DISABLE_SSG_FETCH = 'some-other-value';
+
+          const config = defineConfig({
+            disableStaticPaths: true,
+            ...defaultConfig(),
+          });
+
+          expect(config.disableStaticPaths).to.equal(false);
         });
       });
     });
