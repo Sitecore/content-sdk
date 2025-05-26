26ce3049f119665efadecd82f085e1f584611f56
Addy Pathania <aditya.pathania@sitecore.com>
2025-05-26 17:41:59 -0400

    add command: list

---
+++ packages/cli/src/scripts/auth/list.ts
@@ -0,0 +1,25 @@
+﻿import { CommandModule } from 'yargs';
+import { getAllTenantsInfo } from '../../utils/auth/tenant-store';
+
+export const list: CommandModule = {
+  command: 'list',
+  describe: 'List all tenants',
+  handler: async () => {
+    const tenants = getAllTenantsInfo();
+
+    if (tenants.length === 0) {
+      console.info('\n No tenant info found.');
+      return;
+    }
+
+    console.info('\n Known tenants:');
+    console.table(
+      tenants.map((tenant) => ({
+        tenant_id: tenant.tenantId,
+        tenant_name: tenant.tenantName,
+        organization_id: tenant.organizationId,
+        client_id: tenant.clientId,
+      }))
+    );
+  },
+};
