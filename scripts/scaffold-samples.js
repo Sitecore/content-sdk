5340b58d21e724c672e5a0ec619aff194e9a7a60
illiakovalenko <illia.kovalenko@sitecore.com>
2025-06-23 10:19:41 +0300

    [Chore] Migration to github actions

--- scripts/scaffold-samples.js
+++ scripts/scaffold-samples.js
@@ -8,14 +8,11 @@ for (const sample of samplesToScaffold) {
   const appFolder = getAppFolder(sample.args);
   sample.args.destination = `./samples/${appFolder}`;
   console.log(chalk.green(`Initializing sample ${appFolder} ...`));
-  // we need to keep noInstall as true - otherwise both yarn install and lint will execute
-  // we run lint separately in the azure pipeline
   let scaffoldArgs = {
     template: sample.template,
     yes: true,
     force: true,
     silent: true,
-    noInstall: true,
     ...sample.args,
   };
   initialize(sample.template, scaffoldArgs);
