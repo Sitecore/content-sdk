d4b19347acc78f411990ec0ecd187e0e498e1512
Addy Pathania <aditya.pathania@sitecore.com>
2025-06-27 19:11:09 -0400

    update 19

--- packages/create-content-sdk-app/src/initialize.ts
+++ packages/create-content-sdk-app/src/initialize.ts
@@ -13,6 +13,7 @@ export const initialize = async (template: string, args: BaseAppArgs) => {
   const initializer = await getInitializer(template);
   args.silent || console.log(chalk.cyan(`Initializing '${template}'...`));
   const response = await initializer.init(args);
+  console.log('args', args);
 
   // final steps (install, lint)
   if (!args.noInstall) {
