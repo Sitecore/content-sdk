3164917a53e03fa6723785be29b411031326a877
Illia Kovalenko <23364749+illiakovalenko@users.noreply.github.com>
2025-07-07 17:29:19 +0300

    Merge "Code Extraction" from "beta" branch (#152)
    * [cli] Add code extraction CLI command (#71)
    
    * [Chore] [nextjs] Adjust typescript peerDependency (#82)
    
    * [nextjs][Design Library] Refine code extraction (#104)
    
    * Applied neccessary changes from #113
    
    * [nextjs][Code Extraction] Fix minor bugs (#114)
    
    * [core][nextjs] Add opt-out codegen option in sitecore config (#144)
    
    * Apply neccessary changes from #126
    
    * Updated CHANGELOG
    
    * Updated lock file
    
    * Updated exports
    
    ---------
    
    Co-authored-by: Artem Alexeyenko <ala@sitecore.net>

--- packages/core/src/config/models.ts
+++ packages/core/src/config/models.ts
@@ -192,6 +192,11 @@ export type SitecoreConfigInput = {
      */
     locales?: string[];
   };
+  /**
+   * Opt-out setting for code generation feature
+   * Disables code extraction procedure
+   */
+  disableCodeGeneration?: boolean;
 };
 
 /**
