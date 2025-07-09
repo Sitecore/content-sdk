e85c0789aa03c37d576f34950b52759303854097
Artem Alexeyenko <ala@sitecore.net>
2025-07-09 11:52:41 -0400

    lint test files

--- packages/nextjs/src/tools/codegen/test-data/import-map/mixed.ts
+++ packages/nextjs/src/tools/codegen/test-data/import-map/mixed.ts
@@ -1,5 +1,5 @@
-import { funco} from "./test-exports";
-import { TestClass, testClassInstance as testo } from "./test-exports";
-import * as everything from "./test-exports";
+import { funco } from './test-exports';
+import { TestClass, testClassInstance as testo } from './test-exports';
+import * as everything from './test-exports';
 
 export { funco, testo, TestClass, everything };
