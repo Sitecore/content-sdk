b8906aaa88678d16c1f0ab7e0f6165709858e1a5
Artem Alexeyenko <ala@sitecore.net>
2025-07-28 12:37:57 -0400

    [nextjs] Add default import-map OOB

--- packages/nextjs/src/tools/codegen/import-map.ts
+++ packages/nextjs/src/tools/codegen/import-map.ts
@@ -315,7 +315,7 @@ export const nextJsMapTemplate = (indexedImportMap: Map<string, ModuleExports>) 
           .map(
             (namedExport) => `      { name: '${namedExport.name}', value: ${namedExport.value} }`
           )
-          .join(',\n')
+          .join(',\n') + ','
       : '';
   };
 
