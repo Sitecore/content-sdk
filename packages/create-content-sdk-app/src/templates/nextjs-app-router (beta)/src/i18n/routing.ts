74d1aeafad6dba23cedd252554264d73c977a076
yavorsk <yavor.krastev@sitecore.com>
2025-09-05 15:38:57 +0300

    update next intl files in template

--- packages/create-content-sdk-app/src/templates/nextjs-app-router (beta)/src/i18n/routing.ts
+++ packages/create-content-sdk-app/src/templates/nextjs-app-router (beta)/src/i18n/routing.ts
@@ -1,10 +1,12 @@
-import { defineRouting } from "next-intl/routing";
+import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["en", "de-DE"],
+  locales: ['en', 'de-DE'],
 
   // Used when no locale matches
-  defaultLocale: "en",
-  localePrefix: "as-needed",
+  defaultLocale: 'en',
+
+  // no prefix for default locale 'as-needed' - see next-intl docs for other options https://next-intl.dev/docs/routing/configuration
+  localePrefix: 'as-needed',
 });
