73f8f0fd9a6090f6ea822188aa443fdccf5eb810
yavorsk <yavor.krastev@sitecore.com>
2025-09-11 14:18:25 +0300

    add util for extracting site and locale from header; add not-found.tsx on page level in

--- packages/nextjs/src/utils/index.ts
+++ packages/nextjs/src/utils/index.ts
@@ -1,3 +1,8 @@
-﻿export { handleEditorFastRefresh, extractPath, isServerSidePropsContext } from './utils';
+﻿export {
+  handleEditorFastRefresh,
+  extractPath,
+  isServerSidePropsContext,
+  getSiteAndLocaleFromRewriteHeader,
+} from './utils';
 export { tryParseEnvValue, resolveUrl } from '@sitecore-content-sdk/core/utils';
 export { isEditorActive, resetEditorChromes } from '@sitecore-content-sdk/core/editing';
