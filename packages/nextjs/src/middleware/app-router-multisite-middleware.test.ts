922f97ba9e1cf3bf7ed818ac804a81b69b687380
yavorsk <yavor.krastev@sitecore.com>
2025-09-11 17:29:59 +0300

    remove commented code

--- packages/nextjs/src/middleware/app-router-multisite-middleware.test.ts
+++ packages/nextjs/src/middleware/app-router-multisite-middleware.test.ts
@@ -27,48 +27,6 @@ describe('AppRouterMultisiteMiddleware', () => {
       const result = middleware['getSiteRewrite']('/some/path', 'mysite');
       expect(result).to.equal('/mysite/some/path');
     });
-    //   const req = createRequest({
-    //     headerValues: { host: undefined },
-    //   });
-
-    //   const res = createResponse();
-
-    //   nextRewriteStub = sinon.stub(nextjs.NextResponse, 'rewrite').returns(res);
-
-    //   const { middleware, siteResolver } = createMiddleware();
-
-    //   const finalRes = await middleware.handle(req, res);
-
-    //   validateDebugLog('multisite middleware start: %o', {
-    //     pathname: '/styleguide',
-    //     language: 'en',
-    //     hostname: 'localhost',
-    //   });
-
-    //   validateEndMessageDebugLog('multisite middleware end in %dms: %o', {
-    //     rewritePath: `/${siteName}/styleguide`,
-    //     siteName: siteName,
-    //     headers: {
-    //       'x-sc-rewrite': `/${siteName}/styleguide`,
-    //     },
-    //     cookies: {
-    //       ...res.cookies,
-    //       sc_site: {
-    //         ...defaultSiteCookieAttributes,
-    //         value: siteName,
-    //       },
-    //     },
-    //   });
-
-    //   expect(siteResolver.getByHost).to.be.calledWith('localhost');
-
-    //   expect(finalRes).to.deep.equal(res);
-
-    //   expect(nextRewriteStub).calledWith({
-    //     ...req.nextUrl,
-    //     pathname: `/${siteName}/styleguide`,
-    //   });
-    // });
   });
 
   describe('getAppRouterSiteRewrite', () => {
