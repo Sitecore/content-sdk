0369ca229721bd399661fc077966493f50378981
Addy Pathania <89087450+addy-pathania@users.noreply.github.com>
2025-09-19 13:30:09 -0400

    [react] Wrap generated DL component with chromes metadata (#215)
    * add chromes metadata to generated component
    
    * update changelog

--- packages/react/src/components/DesignLibrary.tsx
+++ packages/react/src/components/DesignLibrary.tsx
@@ -15,6 +15,7 @@ import {
 } from '@sitecore-content-sdk/core/editing';
 import * as codegen from '@sitecore-content-sdk/core/codegen';
 import { useSitecore } from '../enhancers/withSitecore';
+import { PlaceholderMetadata } from './PlaceholderMetadata';
 
 let {
   getDesignLibraryImportMapEvent,
@@ -220,7 +221,9 @@ export const DesignLibrary = ({ loadImportMap }: DesignLibraryProps) => {
     <main>
       {isGeneratedComponentActive ? (
         <ErrorBoundary uid={rendering.uid} renderKey={renderKey}>
-          <Component fields={propsState.fields} params={propsState.params} key={renderKey} />
+          <PlaceholderMetadata rendering={rendering}>
+            <Component fields={propsState.fields} params={propsState.params} key={renderKey} />
+          </PlaceholderMetadata>
         </ErrorBoundary>
       ) : (
         <div id={EDITING_COMPONENT_ID}>
