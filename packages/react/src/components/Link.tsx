6758653be1dd8b18d13ffb10e00ae066a8d3b97f
Addy Pathania <aditya.pathania@sitecore.com>
2025-09-25 15:27:26 -0400

    mark components with use client directive

--- packages/react/src/components/Link.tsx
+++ packages/react/src/components/Link.tsx
@@ -1,4 +1,5 @@
-﻿/* eslint-disable no-unused-expressions, @typescript-eslint/no-unused-expressions */
+﻿'use client';
+/* eslint-disable no-unused-expressions, @typescript-eslint/no-unused-expressions */
 import React, { RefAttributes, forwardRef } from 'react';
 import { FieldMetadata, isFieldValueEmpty } from '@sitecore-content-sdk/core/layout';
 import { withFieldMetadata } from '../enhancers/withFieldMetadata';
--- packages/react/src/components/Placeholder/Placeholder.tsx
+++ packages/react/src/components/Placeholder/Placeholder.tsx
@@ -1,4 +1,5 @@
-﻿import React from 'react';
+﻿'use client';
+import React from 'react';
 import { PlaceholderProps } from './models';
 import { withComponentMap } from '../../enhancers/withComponentMap';
 import { PagesEditor } from '@sitecore-content-sdk/core/editing';
