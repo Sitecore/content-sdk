4eed7ed28dff42af3118762191f7ddab42dc6229
Addy Pathania <aditya.pathania@sitecore.com>
2025-10-16 21:21:10 -0400

    fix lint

--- packages/core/src/tools/codegen/test-data/import-map/fake-react.ts
+++ packages/core/src/tools/codegen/test-data/import-map/fake-react.ts
@@ -1,13 +1,10 @@
+/* eslint-disable jsdoc/require-jsdoc */
 // src/tools/codegen/test-data/import-map/fake-react.ts
 const React = { __fake: true };
 
 export default React;
 
 // named export used by tests/fixtures
-/**
- *
- * @param {...any} _args
- */
 export function useEffect() {
   // noop
 }
