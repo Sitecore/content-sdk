295ccceaec6943c747edf90f7a94ad2d31ca50e6
illiakovalenko <illia.kovalenko@sitecore.com>
2025-11-13 17:27:28 +0200

    Test

--- packages/react/src/components/Placeholder/placeholder-utils.tsx
+++ packages/react/src/components/Placeholder/placeholder-utils.tsx
@@ -169,21 +169,19 @@ export function getAppComponentProps<T extends BasePlaceholderProps>(
  */
 export const getComponentForRendering = (
   renderingDefinition: ComponentRendering,
-  // eslint-disable-next-line no-unused-vars
-  _placeholderName: string,
+  placeholderName: string,
   componentMap?: ComponentMap,
   hiddenRenderingComponent?: React.ComponentClass | React.FC,
   missingComponentComponent?: React.ComponentClass | React.FC
 ): ComponentForRendering => {
-  // eslint-disable-next-line no-unused-vars
-  const logUnknownComponentError = (_variant?: string) => {
-    // console.error(
-    //   `Placeholder ${placeholderName} contains unknown component ${
-    //     renderingDefinition.componentName
-    //   }${
-    //     variant ? ` (${variant})` : ''
-    //   }. Ensure that a React component exists for it, and that it is registered in your component-map file.`
-    // );
+  const logUnknownComponentError = (variant?: string) => {
+    console.error(
+      `Placeholder ${placeholderName} contains unknown component ${
+        renderingDefinition.componentName
+      }${
+        variant ? ` (${variant})` : ''
+      }. Ensure that a React component exists for it, and that it is registered in your component-map file.`
+    );
   };
 
   if (renderingDefinition.componentName === constants.HIDDEN_RENDERING_NAME) {
