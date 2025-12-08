62dcfa63a9e70628ec800294e985e402fcb0b0d2
sc-nikolaoslazaridis <nikolaos.lazaridis@sitecore.com>
2025-12-08 14:40:03 +0200

    chore: jest config name

--- packages/personalize/jest.config.ts
+++ packages/personalize/jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  displayName: 'events',
+  displayName: 'personalize',
   preset: 'ts-jest',
   transform: {
     '^.+\\.ts$': [
