eaca922dcfdfecde0475be2c0fdb1e016ac50d2b
sc-nikolaoslazaridis <nikolaos.lazaridis@sitecore.com>
2026-01-15 13:01:04 +0200

    chore: post merge fix 2

--- packages/nextjs/src/proxy/personalize-proxy.test.ts
+++ packages/nextjs/src/proxy/personalize-proxy.test.ts
@@ -22,7 +22,7 @@ describe('PersonalizeProxy', () => {
   });
 
   const { PersonalizeProxy } = proxyquire('./personalize-proxy', {
-    '@sitecore-cloudsdk/personalize/server': { personalize: CDKPersonalizeStub },
+    '@sitecore-content-sdk/personalize/server': { personalize: CDKPersonalizeStub },
   });
 
   const ua = 'user-agent-string';
