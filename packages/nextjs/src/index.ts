aca5c2ba24b4b5ab6d5127caf73108a60d6917cb
yavorsk <yavor.krastev@sitecore.com>
2026-02-04 15:40:43 +0200

    remove todo comments for feaas and byoc wrappers

--- packages/nextjs/src/index.ts
+++ packages/nextjs/src/index.ts
@@ -116,8 +116,6 @@ export {
   BYOCClientWrapper,
   BYOCServerWrapper,
 } from '@sitecore-content-sdk/react';
-// Preserving original FEaaSWrapper export name for backward compatibility
-// TODO: remove in future major release
 export { FEaaSWrapper };
 export { BYOCWrapper };
 
