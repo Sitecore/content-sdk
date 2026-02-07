464f16ac2e9f84a48a1909c4d4eee02cc03aa565
Stanislav Maxymov <stanislav.maxymov@sitecore.com>
2026-02-07 15:38:58 +0200

    Fix smth

--- samples/astro/src/sitecore.config.ts
+++ samples/astro/src/sitecore.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from '@sitecore-content-sdk/atro';
 export default defineConfig({
   api: {
     edge: {
-      contextId: '2bzCMQpPKU6iZDaWF0eWme',
-      clientContextId: '2bzCMQpPKU6iZDaWF0eWme',
+      contextId: '3w3aG9e6AFKr1asmzeERJa',
+      clientContextId: '3w3aG9e6AFKr1asmzeERJa',
       edgeUrl: 'https://edge-platform-staging.sitecore-staging.cloud',
     },
   },
-  editingSecret: '2bzCMQpPKU6iZDaWF0eWme',
+  editingSecret: '3merRz9GitzaP4gklUDcnR',
   defaultSite: 'site1',
   defaultLanguage: 'en',
 });
