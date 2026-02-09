622d8eb1b3f9418a227039830e72844db40572a6
sc-nikolaoslazaridis <nikolaos.lazaridis@sitecore.com>
2026-02-09 17:24:50 +0200

    chore: add api-extractor to events package

---
+++ packages/events/api/api-surface.d.ts
@@ -0,0 +1,12 @@
+/**
+ * This file is used by API Extractor to analyze the complete API surface
+ * of the core package across all submodules. It should NOT be part of the
+ * actual package exports.
+ */
+
+// Main exports
+export * from '../types/src';
+
+// Submodule exports
+export * from '../browser';
+export * from '../internal';
