2a7b3a04465d828d0b3a9d2c455322e5dc7313f1
illiakovalenko <illia.kovalenko@sitecore.com>
2026-02-11 14:39:22 +0200

    Update

--- packages/core/src/debug.ts
+++ packages/core/src/debug.ts
@@ -50,26 +50,6 @@ export function isNamespaceEnabled(namespace: string): boolean {
 }
 
 /**
- * Converts headers from various formats into a uniform key-value pair object.
- * @param {HeadersInit} incomingHeaders Incoming headers such as a Headers instance or plain object.
- * @returns {Record<string, string | string[]>} Normalized headers as key-value pairs.
- */
-function normalizeHeaders(incomingHeaders: HeadersInit = {}) {
-  const headers: Record<string, string | string[]> = {};
-
-  if (typeof incomingHeaders.forEach === 'function')
-    incomingHeaders.forEach((value, key) => {
-      headers[key] = value;
-    });
-  else
-    Object.entries(incomingHeaders).forEach(([key, value]) => {
-      headers[key] = value;
-    });
-
-  return headers;
-}
-
-/**
  * Default Sitecore Content SDK 'debug' module debuggers. Uses namespace prefix 'content-sdk:'.
  * See {@link https://www.npmjs.com/package/debug} for details.
  */
