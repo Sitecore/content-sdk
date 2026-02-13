df45523e0dbd600e2fe79bef1958891b115ff313
Artem Alexeyenko <ala@sitecore.net>
2026-02-12 20:39:39 -0500

    small refactor for not-placeholders

--- packages/react/src/components/DesignLibrary/DesignLibrary.tsx
+++ packages/react/src/components/DesignLibrary/DesignLibrary.tsx
@@ -156,7 +156,7 @@ export const DesignLibrary = () => {
       cancelled = true;
       unsubscribe && unsubscribe();
     };
-  }, [isVariantGeneration, uid]);
+  }, [isDesignLibrary, isVariantGeneration, uid, loadImportMap, propsState]);
 
   return (
     <main>
