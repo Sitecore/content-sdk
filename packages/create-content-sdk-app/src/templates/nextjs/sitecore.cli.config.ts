e669d0b41ac7e0623c99abb1c07d8715044c154f
Artem Alexeyenko <ala@sitecore.net>
2026-03-02 10:31:01 -0500

    fix nexts import in sample

--- packages/create-content-sdk-app/src/templates/nextjs/sitecore.cli.config.ts
+++ packages/create-content-sdk-app/src/templates/nextjs/sitecore.cli.config.ts
@@ -5,7 +5,7 @@ import {
   generateMetadata,
   extractFiles,
   writeImportMap,
-} from '@sitecore-content-sdk/nextjs/node-tools';
+} from '@sitecore-content-sdk/nextjs/tools';
 
 export default defineCliConfig({
   config: scConfig,
