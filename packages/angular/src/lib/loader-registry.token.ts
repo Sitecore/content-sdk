1faff7f9381364f7fd7234447901a87d158cbc74
Artem Alexeyenko <ala@sitecore.net>
2026-03-05 09:37:18 -0500

    Revert "naive first approach"
    This reverts commit 556e46e535971908a83dd412aadc95e32e60f0c0.

--- packages/angular/src/lib/loader-registry.token.ts
+++
@@ -1,9 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import type { LoaderFn } from './types';
-
-/**
- * Registry of named loaders used for route resolution.
- * Provide a record of loaderId -> LoaderFn so that {@link loaderResolver} can resolve route data.
- * @public
- */
-export const LOADER_REGISTRY = new InjectionToken<Record<string, LoaderFn>>('LOADER_REGISTRY');
