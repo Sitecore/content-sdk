c4eb57dc930444ebb8a5d2dd9ce72563de1d1311
MenKNas <men.nasies@gmail.com>
2026-03-18 17:28:29 +0200

    Adjust atom types to avoid errors

--- packages/react/src/atoms/types.ts
+++ packages/react/src/atoms/types.ts
@@ -28,8 +28,10 @@ export type DefaultChild = AtomMetadata | { atom: AtomMetadata; props?: Record<s
  */
 type PropsOfComponent<C> = C extends (props: infer P) => unknown
   ? P
-  : C extends ComponentType<infer P>
-  ? P
+  : C extends ComponentType<any>
+  ? C extends ComponentType<infer P>
+    ? P
+    : never
   : never;
 
 /** Component props excluding children and ref. @public */
@@ -37,12 +39,13 @@ export type EditableComponentProps<C> = Omit<PropsOfComponent<C>, 'children' | '
 
 /** Keys of T that are callback (function) props. @public */
 export type CallbackPropKeys<T> = {
-  [K in keyof T & string]: NonNullable<T[K]> extends (...args: unknown[]) => unknown ? K : never;
+  [K in keyof T & string]: NonNullable<T[K]> extends (...args: any[]) => unknown ? K : never;
 }[keyof T & string];
 
 /**
  * Tuple of Zod types matching a function's parameter list. Used to type customEvents so each
- * callback's schemas match its parameters.
+ * callback's schemas match its parameters. Keys are strict; tuple length/element strictness
+ * is partial (optional params y?: number infer as undefined and need z.number().optional()).
  * @public
  */
 export type CallbackArgZodTuple<F> = F extends (...args: infer A) => unknown
