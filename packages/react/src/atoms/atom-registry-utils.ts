7d6dc86fe129449132969a78791dd36bb80039c3
yavorsk <yavor.krastev@sitecore.com>
2026-03-25 10:50:34 +0200

    remove unfold atoms function

--- packages/react/src/atoms/atom-registry-utils.ts
+++ packages/react/src/atoms/atom-registry-utils.ts
@@ -7,32 +7,6 @@ const isAtomMetadata = (value: unknown): value is AtomMetadata => {
 };
 
 /**
- * Unfolds the atoms registry by recursively including all child atoms.
- * @param {AtomMetadata[]} atoms - the atoms array to unfold; should contain only top-level atoms (i.e., those with type 'atom')
- * @returns {AtomMetadata[]} the unfolded atoms registry that includes all allowed child atoms
- * @internal
- */
-export const unfoldAtomsRegistry = (atoms: AtomMetadata[]): AtomMetadata[] => {
-  const unfoldedRegistry: AtomMetadata[] = [];
-  const visited = new Set<string>();
-
-  const visit = (atom: AtomMetadata): void => {
-    if (visited.has(atom.name)) {
-      return;
-    }
-
-    visited.add(atom.name);
-    unfoldedRegistry.push(atom);
-
-    (atom.allowedChildren ?? []).filter(isAtomMetadata).forEach(visit);
-  };
-
-  atoms.forEach(visit);
-
-  return unfoldedRegistry;
-};
-
-/**
  * Serializes the provided atoms metadata array into a format suitable for broadcasting via postMessage to the host application
  * @param {AtomMetadata[]} atoms - the atoms to be serialized
  * @returns {AtomInfo[]} the serialized atoms
