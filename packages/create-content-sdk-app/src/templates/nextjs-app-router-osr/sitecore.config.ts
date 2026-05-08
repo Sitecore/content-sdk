fc39bddb4f0d20b8f6149d569ea8c87e63a238f7
MenKNas <men.nasies@gmail.com>
2026-05-08 11:49:52 +0300

    Restore back dictionary caching time

--- packages/create-content-sdk-app/src/templates/nextjs-app-router-osr/sitecore.config.ts
+++ packages/create-content-sdk-app/src/templates/nextjs-app-router-osr/sitecore.config.ts
@@ -4,4 +4,11 @@ import { defineConfig } from '@sitecore-content-sdk/nextjs/config';
  * See the documentation for `defineConfig`:
  * https://doc.sitecore.com/xmc/en/developers/content-sdk/the-sitecore-configuration-file.html
  */
-export default defineConfig({});
+export default defineConfig({
+  dictionary: {
+    caching: {
+      enabled: true,
+      timeout: 60,
+    },
+  },
+});
