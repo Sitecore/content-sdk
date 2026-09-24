25363dce7414d9166e844ce7fc1808dfdf968ab1
Naveen Hedalla Arachchi <nvhd@sitecore.net>
2026-09-24 16:43:35 +0530

    Update packages/content/src/tools/templating/index.ts
    Co-authored-by: Illia Kovalenko <23364749+illiakovalenko@users.noreply.github.com>

--- packages/content/src/tools/templating/index.ts
+++ packages/content/src/tools/templating/index.ts
@@ -9,8 +9,6 @@ export {
   ComponentMapTemplate,
   EnhancedComponentMapTemplate,
   toPascalCase,
-  buildVariantSiblingIndex,
-  isVariantComponent,
 } from './component-builder';
 export { getComponentList } from './components';
 export { prepareComponentsForMap, buildComponentMapContent } from './utils';
